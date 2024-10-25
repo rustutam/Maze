5fc4e609f460ed28549df7c2615b80c1ce932f4c
rustutam <Rustam.Asbapov@urfu.me>
2024-10-25 18:59:06 +0500

    feat: add MazeList and ConvertedMaze models

---
+++ src/main/java/backend/academy/models/ConvertedMazeModel.java
@@ -0,0 +1,9 @@
+package backend.academy.models;
+
+import backend.academy.Coordinate;
+import backend.academy.graph.Graph;
+import backend.academy.graph.Vertex;
+import java.util.Map;
+
+public record ConvertedMazeModel(Graph graph, Map<Coordinate, Vertex> coordinateVertexMap, int height, int width) {
+}
---
+++ src/main/java/backend/academy/models/MazeListModel.java
@@ -0,0 +1,13 @@
+package backend.academy.models;
+
+import backend.academy.Coordinate;
+import backend.academy.cell.Cell;
+import java.util.List;
+import java.util.Map;
+
+public record MazeListModel(
+    Cell[][] mazeList,
+    Map<Coordinate, List<Coordinate>> coordinateNeighboursMap,
+    int height,
+    int width) {
+}
