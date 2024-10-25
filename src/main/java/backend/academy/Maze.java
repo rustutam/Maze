f5091b12a2277f758a83f68ab28a4bca07e38b3c
rustutam <Rustam.Asbapov@urfu.me>
2024-10-26 01:54:10 +0500

    update: do maze to record class

--- src/main/java/backend/academy/Maze.java
+++
@@ -1,9 +0,0 @@
-package backend.academy;
-
-import backend.academy.models.MazeListModel;
-import lombok.Getter;
-
-@Getter public record Maze(int height, int width, MazeListModel mazeListModel) {
-}
-
-
---
+++ src/main/java/backend/academy/models/Maze.java
@@ -0,0 +1,6 @@
+package backend.academy.models;
+
+public record Maze(int height, int width, MazeListModel mazeListModel) {
+}
+
+
